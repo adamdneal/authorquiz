9de4eec9dd16667580749ba1765a729f636940b1
Adam Neal <adamdneal@gmail.com>
2020-04-26 20:38:57 -0500

    Fix spacing.

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -3,52 +3,52 @@ import './App.css';
 import './bootstrap.min.css';
 
 function Hero() {
-  return (<div className="row">
-    <div className="jumbotron col-10 offset-1">
-      <h1>Author Quiz</h1>
-      <p>Select the book written by the author shown</p>
-    </div>
-  </div>);
+    return (<div className="row">
+        <div className="jumbotron col-10 offset-1">
+            <h1>Author Quiz</h1>
+            <p>Select the book written by the author shown</p>
+        </div>
+    </div>);
 }
 
-function Book({title}) {
-  return (<div className="answer">
-    <h4>{title}</h4>
-  </div>);
+function Book({ title }) {
+    return (<div className="answer">
+        <h4>{title}</h4>
+    </div>);
 }
 
 function Turn({ author, books }) {
-  return (<div className="row turn" style={{ backgroundColor: "white" }}>
-    <div className="col-4 offset-1">
-      <img src={author.imageUrl} className="authorimage" alt="Author" />
-    </div>
-    <div className="col-6">
-      {books.map((title) => <Book title={title} key={title} /> )}
-    </div>
-  </div>);
+    return (<div className="row turn" style={{ backgroundColor: "white" }}>
+        <div className="col-4 offset-1">
+            <img src={author.imageUrl} className="authorimage" alt="Author" />
+        </div>
+        <div className="col-6">
+            {books.map((title) => <Book title={title} key={title} />)}
+        </div>
+    </div>);
 }
 
 function Continue() {
-  return (<div />);
+    return (<div />);
 }
 
 function Footer() {
-  return (<div id="footer" className="row">
-    <div className="col-12">
-      <p className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page" target="_blank" rel="noopener noreferrer">Wikimedia Commons</a> and are in the public domain.</p>
-    </div>
-  </div>);
+    return (<div id="footer" className="row">
+        <div className="col-12">
+            <p className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page" target="_blank" rel="noopener noreferrer">Wikimedia Commons</a> and are in the public domain.</p>
+        </div>
+    </div>);
 }
 
 function AuthorQuiz({ turnData }) {
-  return (
-    <div className="container-fluid">
-      <Hero />
-      <Turn {...turnData} />
-      <Continue />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <Hero />
+            <Turn {...turnData} />
+            <Continue />
+            <Footer />
+        </div>
+    );
 }
 
 export default AuthorQuiz;
